a6efbe0d68740c153793338286a8aaac02d3f747
Jake Houston <jakechouston@gmail.com>
2021-02-04 23:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string.toUpperCase);
+  console.log("HELLO");
 }
 function logWhisper(string) {
   console.log(string.toLowerCase);
